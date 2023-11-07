bf49273f2c6e8e9f68600ffcf455dfc34e7f9b23
Dexter Harrison <147558735+dexterharrison29@users.noreply.github.com>
2023-11-07 18:14:55 -0500

    0.1.1 Fixed XOR gates.

--- gates.py
+++ gates.py
@@ -1,20 +1,22 @@
 class LogicGates:
-    def andg(self, i1, i2):
+    def andg(i1, i2):
         if i1==True and i2==True:
             return True
         return False
-    def org(self, i1, i2):
+    def org(i1, i2):
         if i1==True or i2 == True:
             return True
         return False
-    def notg(self, i):
+    def notg(i):
         if i == False:
             return True
         return False
-    def nandg(self, i1, i2):
-        t1 = self.andg(i1, i2)
-        t2 = self.notg(t1)
-    def xorg(self, i1, i2):
-        t1 = self.org(i1, i2)
-        t2 = self.nandg(i1, i2)
-        return(self.andg(t1, t2))
+    def nandg(i1, i2):
+        t1 = LogicGates.andg(i1, i2)
+        t2 = LogicGates.notg(t1)
+    def xorg(i1, i2):
+        t1 = LogicGates.notg(i1)
+        t2 = LogicGates.notg(i2)
+        t3 = LogicGates.andg(i1, t2)
+        t4 = LogicGates.andg(t1, i2)
+        return LogicGates.org(t3, t4)
